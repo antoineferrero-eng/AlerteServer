178a5a0295fa472a5fcf71896fc66fedafa0c21b
p2404056 <p2404056>
2026-05-21 15:01:20 +0200

    Fix: configuration des domaines DuckDNS

--- src/main/java/AlerteServer/config/WebClientConfig.java
+++ src/main/java/AlerteServer/config/WebClientConfig.java
@@ -79,7 +79,7 @@ public class WebClientConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:4200", "http://141.253.96.114", "http://141.253.96.114:*")
+                .allowedOriginPatterns("http://localhost:4200", "http://141.253.96.114", "http://141.253.96.114:*", "https://dacalerte.duckdns.org")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
